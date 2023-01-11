4286e027e39a1ddfed2795e059fcd387e8057e87
Lynne <dev@lynne.ee>
2023-01-11 09:11:23 +0100

    lavc: add h264_vulkan encoder

--- libavcodec/vulkan_video.c
+++ libavcodec/vulkan_video.c
@@ -22,8 +22,8 @@
 
 const FFVkCodecMap ff_vk_codec_map[AV_CODEC_ID_FIRST_AUDIO] = {
     [AV_CODEC_ID_H264] = {
-                           0,
-                           0,
+                           FF_VK_EXT_VIDEO_ENCODE_H264,
+                           VK_VIDEO_CODEC_OPERATION_ENCODE_H264_BIT_EXT,
                            FF_VK_EXT_VIDEO_DECODE_H264,
                            VK_VIDEO_CODEC_OPERATION_DECODE_H264_BIT_KHR,
     },
